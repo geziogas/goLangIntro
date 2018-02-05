87a09384ebf12117b1037928f4ee24c8123fcd83
George Ziogas <gs.ziogas@gmail.com>
2018-02-06 00:10:29 +0100

    Test adding more prints

--- GoWork/src/hello/hello.go
+++ GoWork/src/hello/hello.go
@@ -4,5 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, 世界")
-	fmt.Printf("Hello %s", "world")
+	fmt.Printf("Hello %s\n", "world")
+	fmt.Printf("Hello %s\n", "world")
+	fmt.Printf("Hello %v\n", "world")
+	fmt.Printf("Hello %d", "world")
 }
