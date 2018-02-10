c618b154db250f00375a185d111cafc11e39cf3e
George Ziogas <gs.ziogas@gmail.com>
2018-02-11 00:09:49 +0100

    Trying different functions in Go

--- GoWork/src/hello/hello.go
+++ GoWork/src/hello/hello.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+// var i, j int = 1, 2
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
-	fmt.Printf("Hello %s", "world")
-	fmt.Printf("Hello %s", "world")
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+}
+
+func addNums(x, y int) int {
+	return x + y
+}
+
+func swap(a, b string) (string, string) {
+	return b, a
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
