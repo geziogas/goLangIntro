36276758e0f6b2576d490190fed6acd1a9f51d55
George Ziogas <gs.ziogas@gmail.com>
2018-02-11 00:43:16 +0100

    no message

--- GoWork/src/hello/hello.go
+++ GoWork/src/hello/hello.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"fmt"
-	"math/cmplx"
+	//"math"
 )
 
-// var i, j int = 1, 2
-var (
-	ToBe   bool       = false
-	MaxInt uint64     = 1<<64 - 1
-	z      complex128 = cmplx.Sqrt(-5 + 12i)
+const (
+	// Create a huge number by shifting a 1 bit left 100 places.
+	// In other words, the binary number that is 1 followed by 100 zeroes.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
 )
 
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
+/**
+ *	Binary calculation
+ */
 func main() {
-	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
-	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
-	fmt.Printf("Type: %T Value: %v\n", z, z)
+	var a int = 8
+	var b int = 12
+	fmt.Printf("Before Shifting: %d (%08b),%d (%08b)\n", (a), (a), (b), (b))
+	fmt.Printf("After Shifting: %d (%08b),%d (%08b)", (a << 1), (a << 1), (b << 1), (b << 1))
+	/*fmt.Println(needInt(Small))
+	fmt.Println(needInt(Big))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))*/
 }
 
 func addNums(x, y int) int {
@@ -31,4 +45,8 @@ func split(sum int) (x, y int) {
 	y = sum - x
 	return
 >>>>>>> master
+}
+
+func swapBinary(x, y int) {
+
 }
