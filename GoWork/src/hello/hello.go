7f180d15f3034856eea07748ceb376bfe7a6f613
George Ziogas <gs.ziogas@gmail.com>
2018-02-18 23:26:37 +0100

    For-loop and IFs excercises

--- GoWork/src/hello/hello.go
+++ GoWork/src/hello/hello.go
@@ -9,6 +9,7 @@ const (
 	// Create a huge number by shifting a 1 bit left 100 places.
 	// In other words, the binary number that is 1 followed by 100 zeroes.
 	Big = 1 << 100
+	ab = 0b10101010
 	// Shift it right again 99 places, so we end up with 1<<1, or 2.
 	Small = Big >> 99
 )
@@ -24,8 +25,10 @@ func needFloat(x float64) float64 {
 func main() {
 	var a int = 8
 	var b int = 12
+	fmt.Println(ab)
 	fmt.Printf("Before Shifting: %d (%08b),%d (%08b)\n", (a), (a), (b), (b))
 	fmt.Printf("After Shifting: %d (%08b),%d (%08b)", (a << 1), (a << 1), (b << 1), (b << 1))
+
 	/*fmt.Println(needInt(Small))
 	fmt.Println(needInt(Big))
 	fmt.Println(needFloat(Small))
@@ -44,7 +47,6 @@ func split(sum int) (x, y int) {
 	x = sum * 4 / 9
 	y = sum - x
 	return
->>>>>>> master
 }
 
 func swapBinary(x, y int) {
